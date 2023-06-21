88db3989c9dab9bf2a4733c8e6df53992a8ed314
Charline Mons <charline.mons@gmail.com>
2023-06-21 14:04:14 -0400

    Update icon

--- src/app.js
+++ src/app.js
@@ -10,7 +10,14 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
+  let meridiem = hours >= 12 ? "PM" : "AM"; // Determine AM or PM
+
+  // Convert to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // 0 should be displayed as 12
+
   // get current date
+  let numberDate = now.getDate();
   let days = [
     "Sunday",
     "Monday",
@@ -22,18 +29,36 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
 
-  return `${day} ${hours}:${minutes}`;
+  let months = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+
+  // return `${day}, ${month} ${numberDate} ${hours}:${minutes}`;
+  return `${day}, ${hours}:${minutes} ${meridiem}`;
 }
 
 function displayTemperature(response) {
   console.log(response.data);
-  console.log(response.data.time);
+  console.log(response.data.icon_url);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
@@ -41,9 +66,14 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(timestamp);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", `${response.data.condition.description}`);
 }
 
 let apiKey = "1fabbbt6e694149ea2da3obbe200ebf2";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Montreal&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tokyo&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
